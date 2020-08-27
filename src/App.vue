3c84959568326d35c99c40de820400d7f86cffe5
rohanr <rohanrathan21@gmail.com>
2020-08-27 17:03:40 +0530

    feat: Add carousel and header for landing page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="app" class="bg-gray-900 h-screen flex flex-col"></div>
+  <div id="app" class="bg-gray-900 h-screen flex flex-col">
+    <Header />
+    <Carousel />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Carousel from "./components/Carousel.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+    Carousel,
+  },
 };
 </script>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col pl-16 pt-16 text-gray-300 font-extrabold">
+    <h1 class="text-3xl mb-6">My Playlist</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Carousel",
+};
+</script>
