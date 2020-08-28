c90211229c89b9a3e189c1137a3730f24249a29b
rohanr <rohanrocket4@gmail.com>
2020-08-28 14:14:41 +0530

    refactor: clean up codebase

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,9 +1,8 @@
 <template>
-  <div class="flex flex-col flex-grow h-screen lg:pl-5 lg:py-5 xl:pl-16 xl:pt-16 text-gray-300 font-extrabold max-h-screen">
+  <div
+    class="flex flex-col flex-grow h-screen lg:pl-5 lg:py-5 xl:pl-16 xl:pt-16 text-gray-300 font-extrabold max-h-screen"
+  >
     <MusicPlayer v-model="showModal" />
-    <button class="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease" v-on:click="toggleModal">
-      Open large modal
-    </button>
     <h1 class="text-3xl lg:mb-4 xl:mb-6">My Playlist</h1>
     <div class="w-full lg:h-48 xl:h-64 font-semibold">
       <slider ref="slider" :options="options" @tap="showModal = !showModal">
@@ -12,30 +11,29 @@
           :key="index"
           :style="item.style"
           class="cursor-pointer rounded-lg hover:shadow-lg transition duration-200 transform hover:-translate-y-2 lg:text-xl h-auto select-none"
-          >{{ item.html }}</slideritem
-        >
+        >{{ item.html }}</slideritem>
       </slider>
     </div>
   </div>
 </template>
 
 <script>
-import { slider, slideritem } from 'vue-concise-slider';
-import MusicPlayer from './MusicPlayer.vue';
+import { slider, slideritem } from "vue-concise-slider";
+import MusicPlayer from "./MusicPlayer.vue";
 
 export default {
-  name: 'Carousel',
+  name: "Carousel",
   data() {
     return {
       showModal: false,
       someList: new Array(8).fill({
-        html: 'Hey - Afrojack',
+        html: "Hey - Afrojack",
         style: {
           background:
             "url('https://source.unsplash.com/user/@marcelalaskoski/YrtFlrLo2DQ')",
-          backgroundSize: 'cover',
-          width: '23.5%',
-          marginRight: '0.5%',
+          backgroundSize: "cover",
+          width: "23.5%",
+          marginRight: "0.5%",
         },
       }),
       options: {
@@ -54,10 +52,10 @@ export default {
     slideritem,
     MusicPlayer,
   },
-  methods:{
+  methods: {
     toggleModal() {
       this.showModal = !this.showModal;
-    }
-  }
+    },
+  },
 };
 </script>
--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,42 +1,21 @@
 <template>
   <div v-show="value">
-    <div class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
-      <div class="relative w-auto my-6 mx-auto max-w-6xl">
-        <!--content-->
-        <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-          <!--header-->
-          <div class="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
-            <h3 class="text-3xl font-semibold">
-              Modal Title
-            </h3>
-            <button class="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leadin...rounded-b" />
-            <button class="text-red-500 bg-transparent border border-solid border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease">
-              Close
-            </button>
-            <button @click.prevent="close" class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1" type="button" style="transition: all .15s ease">
-              Save Changes
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
     <div class="opacity-25 fixed inset-0 z-40 bg-black" @click.prevent="close"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MusicPlayer',
+  name: "MusicPlayer",
   props: {
     value: {
-        required: true
-      }
+      required: true,
+    },
   },
-  methods:{
+  methods: {
     close() {
       this.$emit("input", !this.value);
-    }
-  }
-  
+    },
+  },
 };
 </script>
